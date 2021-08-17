4beb2b8250bb8c4479c6d7fec06ee5352ed2c1c1
cassiocmps <cassio.szcampos@gmail.com>
2021-08-17 11:18:52 -0300

    lista 12 ate a 12

--- Lab12 - Functions/ex12/ex12.c
+++ Lab12 - Functions/ex12/ex12.c
@@ -1,12 +1,47 @@
 #include <stdio.h>
 
+typedef struct {
+    double x, y;
+} ponto;
+
+void imprime_ponto(ponto p);
+void Soma(ponto p1, ponto p2, ponto *soma);
+
 int main(){
+    
+    ponto p1, p2, *s, soma;
+    s=&soma;
+
     printf("Digite o valor de x1: ");
+    scanf("%lf", &p1.x);
     printf("Digite o valor de y1: ");
+    scanf("%lf", &p1.y);
+
     printf("Digite o valor de x2: ");
+    scanf("%lf", &p2.x);
     printf("Digite o valor de y2: ");
+    scanf("%lf", &p2.y);
+
+    Soma(p1, p2, s);
+
+    printf("A soma de ");
+    imprime_ponto(p1);
+    printf("com ");
+    imprime_ponto(p2);
+    printf("eh ");
+    imprime_ponto(soma);
+    printf("\n");
 
     return 0;
+}
+
+void imprime_ponto(ponto p){
+    printf("(%.0lf,%.0f) ", p.x, p.y);
+}
+
+void Soma(ponto p1, ponto p2, ponto *soma){
+    soma->x = p1.x + p2.x;
+    soma->y = p1.y + p2.y;
 }
 
 /*

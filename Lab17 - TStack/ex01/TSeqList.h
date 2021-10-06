b65ac3eaf5ff87ad0095dba8bbfa661165d42352
Cassio <cassio.szcampos@gmail.com>
2021-10-06 10:30:08 -0300

    lista 17: ex01

--- Lab17 - TStack/ex01/TSeqList.h
+++ Lab17 - TStack/ex01/TSeqList.h
@@ -2,7 +2,33 @@
 #define _tseqlisth_
 
 #include "aluno.h"
+#define MAX 5
+#define SUCCESS 0
+#define INVALID_NULL_POINTER -1
+#define OUT_OF_MEMORY -2
+#define OUT_OF_RANGE -3
+#define ELEM_NOT_FOUND -4
 
-// code here!
+typedef struct lista TSeqList;
+
+TSeqList* cria_lista();
+int libera_lista(TSeqList* li);
+
+int consulta_lista_pos(TSeqList* li, int pos, struct aluno *al);
+int consulta_lista_mat(TSeqList* li, int mat, struct aluno *al);
+int insere_lista_final(TSeqList* li, struct aluno al);
+int insere_lista_inicio(TSeqList* li, struct aluno al);
+int insere_lista_ordenada(TSeqList* li, struct aluno al);
+int remove_lista(TSeqList* li, int mat);
+int remove_lista_inicio(TSeqList* li);
+int remove_lista_final(TSeqList* li);
+int tamanho_lista(TSeqList* li);
+int lista_cheia(TSeqList* li);
+int lista_vazia(TSeqList* li);
+int imprime_lista(TSeqList* li);
+int remove_lista_otimizado(TSeqList* li, int mat);
+// funções da prova
+int troca_pos_lista(TSeqList *li, int pos1, int pos2);
+int remove_intervalo_lista(TSeqList *li, int posI, int posF);
 
 #endif
--- Lab17 - TStack/ex01/TStack.c
+++ Lab17 - TStack/ex01/TStack.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "TStack.h"
 #include "TSeqList.h"
 
@@ -14,7 +15,7 @@ TStack *stack_create(){
     TStack *st;
     st = malloc(sizeof(TStack));
     if (st != NULL){
-        st->list = list_create();
+        st->list = cria_lista();
         if (st->list == NULL){
             free(st);
             return NULL;
@@ -25,40 +26,65 @@ TStack *stack_create(){
 
 int stack_free(TStack *st)
 {
-
+    if (st == NULL){
+        return INVALID_NULL_POINTER;
+    }
+    libera_lista(st->list);
+    free(st);
+    return 0;
 }
 
 int stack_push(TStack *st, struct aluno al)
 {
-    if (st == NULL)
+    if (st == NULL){
         return INVALID_NULL_POINTER;
-    
-    return list_push_back(st->list, al);
+    }
+    return insere_lista_final(st->list, al);
 
 }
 
 int stack_pop(TStack *st)
 {
-
+    if (st == NULL){
+        return INVALID_NULL_POINTER;
+    }
+    return remove_lista_final(st->list);
 }
 
 int stack_top(TStack *st, struct aluno *al)
 {
-
+    if (st == NULL){
+        return INVALID_NULL_POINTER;
+    }
+    int last_pos=(tamanho_lista(st->list)-1);
+    return consulta_lista_pos(st->list, last_pos, al);
 }
 
 int stack_empty(TStack *st)
 {
-
+    if (st == NULL){
+        return INVALID_NULL_POINTER;
+    }
+    return lista_vazia(st->list);
 }
 
 int stack_full(TStack *st)
 {
-
+    if (st == NULL){
+        return INVALID_NULL_POINTER;
+    }
+    return lista_cheia(st->list);
 }
 
 int stack_print(TStack *st)
 {
+    if (st == NULL){
+        return INVALID_NULL_POINTER;
+    }
 
+    printf("\nPRINTING STACK...\n");
+    imprime_lista(st->list);
+    printf("\nEND OF STACK.\n");
+    return 0;
 }
 
